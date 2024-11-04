7ff279784282f7c02a6a824abc9803c771ed1a99
bruno b bueno <intratechinfo@gmail.com>
2024-11-04 15:39:38 -0300

    criar função sortear

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+    
+    alert(`Quantidade de numeros: ${quantidade}`)
+    alert(`De numero: ${de}`)
+    alert(`Até o número: ${ate}`)
+}
